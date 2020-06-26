3bcf4c2845d124108466fe375e7cb1bffab3e749
Sarthak Gupta <saarthak19445@iiitd.ac.in>
2020-06-26 15:08:13 +0530

    Added paddles and ball

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 push = require 'push'
+TLfres = require 'tlfres'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -9,6 +10,9 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -24,7 +28,20 @@ end
 
 function love.draw()
     push:apply('start')
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    -- LÖVE 11.0 uses decimal values between 0-1
+    -- as opposed to LÖVE 10.2 which uses 0-255
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- x & y coordinates of the rectangle correspond to 
+    -- the TOP-LEFT point rather than the center
+
+    -- left paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    -- right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply('end')
 end
