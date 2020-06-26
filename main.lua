9907b5d83f19cfcb162bd6ed10363275fda52047
Sarthak Gupta <saarthak19445@iiitd.ac.in>
2020-06-26 17:18:56 +0530

    Version3: Paddle update

--- main.lua
+++ main.lua
@@ -9,13 +9,41 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- movement speed of paddle
+-- multiplied by dt during update
+-- to match frame rates across systems
+PADDLE_SPEED = 200
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    love.graphics.setFont(smallFont)
+    -- font for displaying score of players
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    -- initializing player scores
+    player1Score = 0
+    player2Score = 0
+
+    -- initial paddle positions (along Y-axis)
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 function love.keypressed(key)
@@ -30,15 +58,26 @@ function love.draw()
     -- as opposed to LÖVE 10.2 which uses 0-255
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.printf('Hello Pong!', smallFont, 0, 5, VIRTUAL_WIDTH, 'center')
+
+    -- draw scores on the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(
+        tostring(player1Score),
+        VIRTUAL_WIDTH / 2 - 50,
+        VIRTUAL_HEIGHT / 15)
+    love.graphics.print(
+        tostring(player2Score),
+        VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 15)
 
     -- x & y coordinates of the rectangle correspond to 
     -- the TOP-LEFT point rather than the center
-
     -- left paddle
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
     -- right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
     -- ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
