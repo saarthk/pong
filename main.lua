72974f9975d996f0d142992da55561f2062df7e6
Sarthak Gupta <saarthak19445@iiitd.ac.in>
2020-06-28 14:32:29 +0530

    [UPDATE 10] Winner update

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ TLfres = require 'tlfres'
 require 'Paddle'
 require 'Ball'
 
--- windows resizes the display to 150% of the original
+-- Windows resizes the display to 150% of the original
 -- so the maximum possible resolution without overflow
 -- can be 1280x720, instead of 1920x1080
 WINDOW_WIDTH = 1280
@@ -25,8 +25,11 @@ function love.load()
     math.randomseed(os.time())
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    smallFont = love.graphics.newFont('font.ttf', 8)
 
+    -- default font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    -- font for displaying winning message
+    largeFont = love.graphics.newFont('font.ttf', 16)
     -- font for displaying score of players
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
@@ -37,9 +40,10 @@ function love.load()
     -- represents the player serving the ball
     servingPlayer =1
 
+    -- instantiate players
     player1 = Paddle:init(5, 30, 5, 50)
     player2 = Paddle:init(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 50)
-
+    -- instantiate ball
     ball = Ball:init(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- game state variable used to transition between different parts of the game
@@ -64,7 +68,7 @@ function love.update(dt)
             ball.x = player1.x + 5
             -- x component of the velocity is reversed
             -- and the ball is accelerated in x
-            ball.dx = -ball.dx * 1.20
+            ball.dx = -ball.dx * 10.5
 
             -- direction of the y component of velocity is maintained
             -- but the magnitude is changed at random
@@ -78,7 +82,7 @@ function love.update(dt)
         -- same as player 1's collision detection
         if ball:collide(player2) then
             ball.x = player2.x - 4
-            ball.dx = -ball.dx * 1.10
+            ball.dx = -ball.dx * 10.5
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
             else
@@ -86,32 +90,42 @@ function love.update(dt)
             end
         end
 
+
         -- collision detection along the ceiling and floor
         -- using simple math :P
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
-        end
 
-        if ball.y >= VIRTUAL_HEIGHT - 4 then
+        elseif ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+        end    
+
+        if ball.x < 0 then
+            servingPlayer = 1
+            player2Score = player2Score + 1
+
+            if player2Score == 10 then
+                winningPlayer = 2
+                gameState = 'end'
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
         end
-    end
-    
 
-    if ball.x < 0 then
-        servingPlayer = 1
-        player2Score = player2Score + 1
-        ball:reset()
-        gameState = 'serve'
-    end
-
-    if ball.x > VIRTUAL_WIDTH then
-        servingPlayer = 2
-        player1Score = player1Score + 1
-        ball:reset()
-        gameState = 'serve'
+        if ball.x > VIRTUAL_WIDTH then
+            servingPlayer = 2
+            player1Score = player1Score + 1
+            if player1Score == 10 then
+                winningPlayer = 1
+                gameState = 'end'
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
+        end
     end
 
 
@@ -152,6 +166,23 @@ function love.keypressed(key)
             gameState = 'serve'
         elseif gameState == 'serve' then
             gameState = 'play'
+        elseif gameState == 'end' then
+            -- on pressing enter after finishing the game
+            -- game restarts in the serving position
+            gameState = 'serve'
+            -- and all objects are reset
+            ball:reset()
+
+            player1Score = 0
+            player2Score = 0
+
+            -- decide who serves first
+            -- depending upon the result of the previous game
+            if winningPlayer == 1 then
+                servingPlayer = 2
+            else
+                servingPlayer = 1
+            end
         end
     end
 end
@@ -163,14 +194,24 @@ function love.draw()
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     love.graphics.setColor(1, 1, 1)
 
+    displayScore()
+
     if gameState == 'start' then
         love.graphics.printf(
             'Welcome to Pong!', 
             smallFont, 
-            10, 20, VIRTUAL_WIDTH - 10, 'left')
+            0, 0, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf(
+            'Press ENTER to start',
+            smallFont,
+            0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.line(VIRTUAL_WIDTH / 2, 30, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT)
 
     elseif gameState == 'serve' then
-        love.graphics.printf('SERVE', smallFont, 10, 20, VIRTUAL_WIDTH - 10, 'left')
+        love.graphics.printf(
+            'SERVE', 
+            smallFont, 
+            10, 20, VIRTUAL_WIDTH - 10, 'left')
         love.graphics.printf(
             'Player ' .. tostring(servingPlayer) .. "'s serve!",
             smallFont,
@@ -178,31 +219,40 @@ function love.draw()
             )
         love.graphics.printf(
             'Press ENTER to serve',
+            smallFont,
             0, 10, VIRTUAL_WIDTH, 'center')
         love.graphics.line(VIRTUAL_WIDTH / 2, 30, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT)
 
     elseif gameState == 'play' then
-        love.graphics.printf('PLAY', smallFont, 10, 20, VIRTUAL_WIDTH - 10, 'left')
+        love.graphics.printf(
+            'PLAY', 
+            smallFont, 
+            10, 20, VIRTUAL_WIDTH - 10, 'left')
         love.graphics.line(VIRTUAL_WIDTH / 2, 0, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT)
+
+    elseif gameState == 'end' then
+        love.graphics.printf(
+            'END', 
+            smallFont, 
+            10, 20, VIRTUAL_WIDTH - 10, 'left')
+        love.graphics.printf(
+            'Player ' .. tostring(winningPlayer) .. ' wins!',
+            largeFont,
+            0, 0, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf(
+            'Press ENTER to restart',
+            smallFont,
+            0, 16
+            , VIRTUAL_WIDTH, 'center')
+        love.graphics.line(VIRTUAL_WIDTH / 2, 30, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT)
     end
 
     -- love.graphics.circle('fill', VIRTUAL_WIDTH / 2 + 6, VIRTUAL_HEIGHT / 2 + 6, 3)
 
     player1:render()
     player2:render()
-
     ball:render()
-    -- draw scores on the screen
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(
-        tostring(player1Score),
-        VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 10)
-    love.graphics.print(
-        tostring(player2Score),
-        VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 10)
-
+    
     -- set font properties
     love.graphics.setFont(smallFont)
     love.graphics.setColor(0, 1, 0, 1)
@@ -213,4 +263,17 @@ function love.draw()
         )
 
     TLfres.endRendering()
+end
+
+function displayScore()
+    -- draw scores on the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(
+        tostring(player1Score),
+        VIRTUAL_WIDTH / 2 - 50,
+        VIRTUAL_HEIGHT / 10)
+    love.graphics.print(
+        tostring(player2Score),
+        VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 10)
 end
